0adaee2b5394145b73c721e0a56166c4a2255bec
gemmaro <gemmaro.dev@gmail.com>
2025-04-08 20:29:39 +0900

    Use Ruby 3.1.

--- manifest.scm
+++ manifest.scm
@@ -1,11 +1,2 @@
-(use-modules (guix packages)
-             (gnu packages ruby))
-
-(define-public ruby-nokogiri'
-  (package
-   (inherit ruby-nokogiri)
-   (arguments
-    (cons* #:ruby ruby-3.2
-           (package-arguments ruby-nokogiri)))))
-
-(packages->manifest (list ruby-nokogiri' ruby-rspec ruby-3.2 ruby-rubocop))
+(use-modules (gnu packages ruby))
+(packages->manifest (list ruby-nokogiri ruby-rspec ruby-3.1 ruby-rubocop))
